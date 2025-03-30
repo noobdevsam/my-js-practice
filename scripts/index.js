a344f2a307fc5c4ee2d9390a656fa53d9ccac360
noobdevsam <mdshoykotislam05@gmail.com>
2025-03-30 12:56:33 +0600

    correcting argument of the write method

--- scripts/index.js
+++ scripts/index.js
@@ -7,7 +7,7 @@ document.getElementById("demo2").innerText = "This is a paragraph";
 
 // Using document.write()
 document.write("Hello World!\n");
-document.write(5 + 6);
+document.write((5 + 6).toString());
 
 // Using window.alert()
 // Specifying the [window] object is optional
