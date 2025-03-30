ff963c98941167a260f4387eedabb95d4e1793c6
noobdevsam <mdshoykotislam05@gmail.com>
2025-03-30 12:21:44 +0600

    moving js code to a separate file

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+// Using innerHTML
+document.getElementById("demo").innerHTML = "Hello JavaScript! changed text";
+
+document.getElementById("change-to-h3").innerHTML = "<h3>This is a heading</h3>";
+
+// Using document.write()
+document.write("Hello World!");
+
+// Using window.alert()
+window.alert("Hello World!");
+
+// Using console.log()
+console.log("Hello World!");
