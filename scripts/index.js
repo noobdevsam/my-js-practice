af7c088b2d44a757769f50769aa8108e5c74ceae
noobdevsam <mdshoykotislam05@gmail.com>
2025-03-30 12:52:33 +0600

    using alert, print, write, innerText functionalities

--- scripts/index.js
+++ scripts/index.js
@@ -3,10 +3,14 @@ document.getElementById("demo").innerHTML = "Hello JavaScript! changed text";
 
 document.getElementById("change-to-h3").innerHTML = "<h3>This is a heading</h3>";
 
+document.getElementById("demo2").innerText = "This is a paragraph";
+
 // Using document.write()
-document.write("Hello World!");
+document.write("Hello World!\n");
+document.write(5 + 6);
 
 // Using window.alert()
+// Specifying the [window] object is optional
 window.alert("Hello World!");
 
 // Using console.log()
